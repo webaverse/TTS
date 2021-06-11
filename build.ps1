207ff32ae605dc9f3b0536115dfb4fe047eb82bc
Avaer Kazmer <a@modules.io>
2021-06-11 12:12:41 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,8 @@
 .\Scripts\python .\TTS\bin\compute_statistics.py --config_path config.json --out_path scale_stats.npy
 .\Scripts\python .\TTS\bin\train_tacotron.py --config_path config.json
+
+pushd /mnt/c/Users/avaer/Documents/Image-Line/FL\ Studio/Audio/Rendered/;
+for f in *; do
+  echo -i "$f" -ac 1 wavs/${f%%.*}.wav
+done;
+podp;
